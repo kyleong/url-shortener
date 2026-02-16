3e6060dab073367c44fd252fc43aaa849e85676d
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2026-02-16 17:42:41 +0800

    feat: implement Url model with validations and short code generation

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,16 @@
 class Url < ApplicationRecord
+  validates :target_url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: "must be a valid HTTP or HTTPS URL" }
+  validates :short_code, uniqueness: true
+  after_create :generate_short_code
+
+  def to_param
+    short_code
+  end
+
+  private
+  def generate_short_code
+    return if short_code.present?
+    short_code = ShortCodeGenerator.generate(id)
+    update_column(:short_code, short_code)
+  end
 end
