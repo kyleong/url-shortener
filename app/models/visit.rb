c48176a6d770b8f715fe710738438ceb7bab22f6
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2026-02-18 11:27:56 +0800

    feat: turbo stream visits

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,3 +1,16 @@
 class Visit < ApplicationRecord
   belongs_to :url
+
+  after_create_commit :broadcast_visit
+
+  private
+
+  def broadcast_visit
+    return unless url.present?
+
+    broadcast_prepend_to url.id,
+      target: "visits",
+      partial: "urls/visit",
+      locals: { visit: self }
+  end
 end
