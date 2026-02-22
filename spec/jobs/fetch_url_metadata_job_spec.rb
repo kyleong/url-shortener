480660f4c2c1ddf4390b661e0cd2bf45a6dd5458
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2026-02-22 20:43:44 +0800

    feat: add RSpec tests for FetchUrlMetadataJob to validate URL metadata fetching and handling

---
+++ spec/jobs/fetch_url_metadata_job_spec.rb
@@ -0,0 +1,63 @@
+require "rails_helper"
+
+RSpec.describe FetchUrlMetadataJob, type: :job do
+  describe "#perform" do
+    let(:url) { Url.create!(target_url: "https://example.com") }
+
+    it "updates the url with fetched metadata" do
+      allow_any_instance_of(FetchUrlMetadataJob).to receive(:fetch_url_metadata)
+        .with(url.target_url)
+        .and_return(status_code: 200, title: "Example Title")
+
+      described_class.perform_now(url.id)
+
+      url.reload
+      expect(url.fetch_status_code).to eq(200)
+      expect(url.title).to eq("Example Title")
+      expect(url.fetched_at).to be_present
+    end
+
+    it "does not update when metadata is nil" do
+      allow_any_instance_of(FetchUrlMetadataJob).to receive(:fetch_url_metadata)
+        .with(url.target_url)
+        .and_return(nil)
+
+      described_class.perform_now(url.id)
+
+      url.reload
+      expect(url.fetch_status_code).to be_nil
+      expect(url.title).to be_nil
+      expect(url.fetched_at).to be_nil
+    end
+  end
+
+  describe "#fetch_url_metadata" do
+    let(:job) { described_class.new }
+
+    it "returns status code and title for a successful response" do
+      http_double = double(get: instance_double(Net::HTTPOK, code: "200", body: "<title>Example</title>"))
+      allow(Net::HTTP).to receive(:start).and_yield(http_double)
+
+      result = job.send(:fetch_url_metadata, "https://example.com")
+      expect(result).to eq(status_code: 200, title: "Example")
+    end
+
+    it "follows redirects" do
+      redirect_response = instance_double(Net::HTTPRedirection, code: "301", body: nil)
+      ok_response = instance_double(Net::HTTPOK, code: "200", body: "<title>Redirected</title>")
+
+      allow(redirect_response).to receive(:is_a?).with(Net::HTTPRedirection).and_return(true)
+      allow(redirect_response).to receive(:[]).with("location").and_return("https://example.org")
+      allow(ok_response).to receive(:is_a?).with(Net::HTTPRedirection).and_return(false)
+
+      http_double = double
+      allow(http_double).to receive(:get)
+        .and_return(redirect_response, ok_response)
+
+      allow(Net::HTTP).to receive(:start).and_yield(http_double)
+
+      result = job.send(:fetch_url_metadata, "https://example.com")
+      expect(result).to eq(status_code: 200, title: "Redirected")
+    end
+  end
+end
