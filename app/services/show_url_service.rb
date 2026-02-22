1f63dc757c0c51d036aac95d5043633f9f5c0e99
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2026-02-22 17:28:46 +0800

    feat: implement ShowUrlService for paginating URL visits

---
+++ app/services/show_url_service.rb
@@ -0,0 +1,21 @@
+class ShowUrlService < ApplicationService
+  PER_PAGE = 5
+
+  def initialize(url:, page:)
+    @url = url
+    @page = page.to_i <= 0 ? 1 : page.to_i
+  end
+
+  def call
+    offset = (@page - 1) * PER_PAGE
+
+    visits = @url.visits
+    .order(created_at: :desc)
+    .limit(PER_PAGE)
+    .offset(offset)
+
+    next_page = @url.visits.count > @page * PER_PAGE ? @page + 1 : nil
+
+    [ visits, next_page ]
+  end
+end
---
+++ spec/services/show_url_service_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe ShowUrlService do
+  describe "#call" do
+    let(:url) { create(:url) }
+
+    it "returns nil next page when there are fewer visits than the page size" do
+      create_list(:visit, 3, url: url)
+      visits, next_page = described_class.new(url: url, page: 1).call
+      expect(visits.count).to eq(3)
+      expect(next_page).to eq(nil)
+    end
+
+    context "when visits is more than the page size" do
+      it "paginates visits and returns next page when more results exist" do
+        create_list(:visit, 7, url: url)
+        visits, next_page = described_class.new(url: url, page: 1).call
+        expect(visits.count).to eq(5)
+        expect(next_page).to eq(2)
+      end
+
+      it "returns nil next page when on the last page" do
+        create_list(:visit, 7, url: url)
+        visits, next_page = described_class.new(url: url, page: 2).call
+        expect(visits.count).to eq(2)
+        expect(next_page).to eq(nil)
+      end
+    end
+  end
+end
