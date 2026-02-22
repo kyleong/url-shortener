238f697831d86ae00923964c8ddae8fbde0d86b8
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2026-02-22 14:44:43 +0800

    feat: implement toast notifications for copy actions across URL components

---
+++ app/javascript/controllers/toast_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  show(event) {
+    const message = event?.params?.message || "";
+
+    const toastContainer = document.getElementById("toast-container");
+
+    const alert = document.createElement("div");
+    alert.classList.add("alert", "alert-sm", "shadow-xl");
+
+    alert.innerHTML = message;
+    toastContainer.appendChild(alert);
+
+    setTimeout(() => {
+      alert.classList.add(
+        "opacity-0",
+        "translate-x-4",
+        "duration-300",
+        "transition-all",
+      );
+      setTimeout(() => {
+        alert.remove();
+      }, 300);
+    }, 3000);
+  }
+}
