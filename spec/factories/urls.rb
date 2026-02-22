018d03954d207116834e37fdc5789594859f655c
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2026-02-22 17:28:36 +0800

    feat: add factories for URL and Visit models to support testing

---
+++ spec/factories/urls.rb
@@ -0,0 +1,26 @@
+# spec/factories/urls.rb
+FactoryBot.define do
+  factory :url do
+    sequence(:target_url) { |n| "https://example#{n}.com/some/path" }
+    sequence(:short_code) { |n| "abc#{n.to_s.rjust(3, '0')}" }
+    title { "Example Page Title" }
+    is_active { true }
+    session_id { SecureRandom.hex(16) }
+    fetch_status_code { 200 }
+    fetched_at { 1.hour.ago }
+
+    trait :inactive do
+      is_active { false }
+    end
+
+    trait :unfetched do
+      fetch_status_code { nil }
+      fetched_at { nil }
+      title { nil }
+    end
+
+    trait :not_found do
+      fetch_status_code { 404 }
+    end
+  end
+end
---
+++ spec/factories/visits.rb
@@ -0,0 +1,32 @@
+# spec/factories/visits.rb
+FactoryBot.define do
+  factory :visit do
+    association :url
+
+    visited_at { Time.current }
+    ip_address { Faker::Internet.ip_v4_address }
+    user_agent { Faker::Internet.user_agent }
+    referer { Faker::Internet.url }
+    city { Faker::Address.city }
+    country { Faker::Address.country }
+    country_code { Faker::Address.country_code }
+    latitude { Faker::Address.latitude }
+    longitude { Faker::Address.longitude }
+
+    trait :no_location do
+      city { nil }
+      country { nil }
+      country_code { nil }
+      latitude { nil }
+      longitude { nil }
+    end
+
+    trait :no_referer do
+      referer { nil }
+    end
+
+    trait :ipv6 do
+      ip_address { Faker::Internet.ip_v6_address }
+    end
+  end
+end
