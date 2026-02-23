999539d2e306d5ddca124ec4bf05e51a1ee4917a
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2026-02-23 22:32:40 +0800

    chore: configure Sidekiq as the active job queue adapter and add initializer

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,7 @@
+Sidekiq.configure_server do |config|
+  config.redis = { url: "redis://#{ENV.fetch('REDIS_HOST', 'localhost')}:#{ENV.fetch('REDIS_PORT', '6379')}/0" }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: "redis://#{ENV.fetch('REDIS_HOST', 'localhost')}:#{ENV.fetch('REDIS_PORT', '6379')}/0" }
+end
