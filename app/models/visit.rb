739d391b9b258b59da105d6f17f4685d2520d9b0
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2026-02-24 17:25:57 +0800

    feat: broadcast to replace url on update

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,11 +1,12 @@
 class Visit < ApplicationRecord
   belongs_to :url
 
-  after_create_commit :broadcast_visit
+  after_create_commit :broadcast_url
+  after_update_commit :broadcast_visit
 
   private
 
-  def broadcast_visit
+  def broadcast_url
     return unless url.present?
 
     broadcast_prepend_to url.id,
@@ -16,5 +17,13 @@ class Visit < ApplicationRecord
     broadcast_update_to url.id,
       target: "visit_count",
       html: "#{url.visits.count} clicks"
+  end
+
+  def broadcast_visit
+    return unless url.present?
+    broadcast_replace_to url.id,
+      target: ActionView::RecordIdentifier.dom_id(self),
+      partial: "urls/visit",
+      locals: { visit: self }
   end
 end
