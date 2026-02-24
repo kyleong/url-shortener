e8200ac125581791c505047abacc179a7a7255f2
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2026-02-24 17:26:22 +0800

    test: add test for broadcast visit and refactor test for broadcast url

--- spec/models/visit_spec.rb
+++ spec/models/visit_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Visit, type: :model do
   end
 
   describe "callbacks" do
-    describe "#broadcast_visit" do
+    describe "#broadcast_url" do
       it "broadcasts the new visit and updates the visit count" do
         expect_any_instance_of(Visit).to receive(:broadcast_prepend_to).with(
           url.id,
@@ -26,6 +26,22 @@ RSpec.describe Visit, type: :model do
         )
 
         create(:visit, url: url)
+      end
+    end
+
+    describe "#broadcast_visit" do
+      it "broadcasts the updated visit" do
+        visit = create(:visit, url: url)
+        target = ActionView::RecordIdentifier.dom_id(visit)
+
+        expect(visit).to receive(:broadcast_replace_to).with(
+          url.id,
+          target: target,
+          partial: "urls/visit",
+          locals: { visit: visit }
+        )
+
+        visit.update(updated_at: Time.current)
       end
     end
   end
