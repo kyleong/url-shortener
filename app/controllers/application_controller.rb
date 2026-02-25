b705bc22cbe67f504932462264c6296f846e22b6
Kyle Ong <34449052+KyleOng@users.noreply.github.com>
2026-02-25 19:03:12 +0800

    feat: add permissions policy header to enhance security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :set_permissions_policy
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  def set_permissions_policy
+    response.headers["Permissions-Policy"] = "camera=(), microphone=(), geolocation=(), payment=(), usb=(), interest-cohort=()"
+  end
 end
